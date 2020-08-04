610fb795e1384100f7737f1385b435d384da6170
jjw4145 <jjw4145@naver.com>
2020-08-04 21:08:30 +0900

    modify songs.controller.js

--- nodejs-backend/src/api/songs/songs.controller.js
+++ nodejs-backend/src/api/songs/songs.controller.js
@@ -9,7 +9,27 @@ exports.list = async (ctx) => {
     }
 
     ctx.body = songs; 
-}
+};
+
+exports.get = async (ctx) => {
+    const { id } = ctx.params;
+
+    let song;
+
+    try{
+        song = await Song.findById(id).exec();
+    } catch(e) {
+        return ctx.throw(500, e);
+    }
+
+    if(!song) {
+        ctx.status = 404;
+        ctx.body = { message : 'song not found' };
+        return;
+    }
+
+    ctx.body = song;
+};
 
 exports.create = async (ctx) => {
     const {
@@ -33,14 +53,42 @@ exports.create = async (ctx) => {
     ctx.body = song;
 }
 
-exports.delete = (ctx) => {
-    ctx.body = 'deleted';
-}
+exports.delete = async (ctx) => {
+    const { id } = ctx.params;
+
+    try {
+        await Book.findByIdAndRemove(id).exec();
+    } catch (e) {
+        if(e.name === 'CastError') {
+            ctx.status = 400;
+            return;
+        }
+    }
+
+    ctx.status = 204;
+};
 
 exports.replace = (ctx) => {
-    ctx.body = 'replaced';
-}
+   ctx.body = 'replaced';
+};
 
-exports.update = (ctx) => {
-    ctx.body = 'updated';
-}
+exports.update = async (ctx) => {
+    const { id } = ctx.params;
+
+    if(!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        return;
+    }
+
+    let song;
+
+    try {
+        song = await Song.findByIdAndUpdate(id, ctx.request.body, {
+            new: true
+        });
+    } catch (e) {
+        return ctx.throw(500, e);
+    }
+
+    ctx.body = song;
+};
